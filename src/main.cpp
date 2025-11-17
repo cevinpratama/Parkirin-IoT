0726433c2091fbfc8c11405b7b01b6183b89985d
Cevin Pratama Sundora <cevinpubg8@gmail.com>
2025-11-17 20:51:21 +0700

    feat(main): add ping sensor testing

--- src/main.cpp
+++ src/main.cpp
@@ -5,19 +5,37 @@
 #include <addons/TokenHelper.h> 
 #include "secrets.h" 
 
-// put function declarations here:
-int myFunction(int, int);
+const int TRIG_PIN = D5;
+const int ECHO_PIN = D6; 
+long duration;
+int distance;
+
+
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+
+  // Baca durasi pulsa Echo
+  duration = pulseIn(ECHO_PIN, HIGH);
+
+  // Hitung jarak (cm)
+  distance = duration * 0.0343 / 2;
+
+  // Tampilkan hasil
+  Serial.print("Jarak: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+
+  delay(500);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
